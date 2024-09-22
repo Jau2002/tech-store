9f57422bbfb170fb5fd5bf8351deff7cb15be839
Calidad <calidad@enternova.com.co>
2024-09-22 17:39:54 -0500

    module cuentas

--- src/main/webapp/js/loggedTest.js
+++ src/main/webapp/js/loggedTest.js
@@ -1,7 +1,53 @@
-import { IdIsLogged } from './selectors'
-import { validations } from './validations'
+const $ = (select) => document.querySelector(select)
 
-const { email, psw, submitButton } = new IdIsLogged()
+class IdIsLogged {
+	constructor() {
+		this.email = $('#email')
+		this.psw = $('#psw')
+		this.submitButton = $('#submit')
+		this.pswError = $('#passwordHelpBlock')
+		this.emailError = $('#emailHelpBlock')
+	}
+}
+
+const { email, psw, submitButton, emailError, pswError } = new IdIsLogged()
+
+function validateIsLogged() {
+	let isValid = true
+
+	if (!email.value) {
+		email.focus()
+		emailError.style.display = 'block'
+		isValid = false
+	} else if (!/^[a-z]+@unbosque\.edu\.co$/.test(email.value)) {
+		email.focus()
+		emailError.style.display = 'block'
+		isValid = false
+	} else {
+		emailError.style.display = 'none'
+	}
+
+	if (isValid != false && !psw.value) {
+		psw.focus()
+		isValid = false
+		pswError.style.display = 'block'
+	} else if (
+		isValid != false &&
+		!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,20}$/.test(psw.value)
+	) {
+		psw.focus()
+		isValid = false
+		pswError.style.display = 'block'
+	} else {
+		pswError.style.display = 'none'
+	}
+
+	return isValid
+}
+
+const validations = {
+	isLogged: () => (submitButton.disabled = !validateIsLogged()),
+}
 
 document.addEventListener('DOMContentLoaded', () => {
 	email.addEventListener('input', validations.isLogged)
--- src/main/webapp/js/registerTest.js
+++ src/main/webapp/js/registerTest.js
@@ -1,7 +1,71 @@
-import { IdIsRegister } from './selectors'
-import { validations } from './validations'
+const $ = (select) => document.querySelector(select)
 
-const { email, psw, submitButton, newPsw } = new IdIsRegister()
+class IdIsLogged {
+	constructor() {
+		this.email = $('#email')
+		this.psw = $('#psw')
+		this.submitButton = $('#submit')
+		this.pswError = $('#passwordHelpBlock')
+		this.emailError = $('#emailHelpBlock')
+	}
+}
+
+const { email, psw, submitButton, newPsw, emailError } = new IdIsLogged()
+
+const passwordValidation = (pass, err) => {
+	let isValid = true
+
+	if (isValid != false && !pass.value) {
+		pass.focus()
+		isValid = false
+		err.style.display = 'block'
+	} else if (
+		isValid != false &&
+		!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,20}$/.test(pass.value)
+	) {
+		pass.focus()
+		isValid = false
+		err.style.display = 'block'
+	} else {
+		err.style.display = 'none'
+	}
+	return isValid
+}
+
+const passwordConfirmValidation = (pass, compare, err) => {
+	let isValid = false
+	
+	if (isValid != false && !pass.value) {
+		pass.focus()
+		isValid = false
+		err.style.display = 'block'
+	} else if (isValid != false && pass.value != compare.value) {
+		pass.focus()
+		isValid = false
+		err.style.display = 'block'
+	} else {
+		err.style.display = 'none'
+	}
+	return isValid
+}
+
+function validateIsLogged() {
+	let isValid = true
+
+	if (!email.value) {
+		email.focus()
+		emailError.style.display = 'block'
+		isValid = false
+	} else if (!/^[a-z]+@unbosque\.edu\.co$/.test(email.value)) {
+		email.focus()
+		emailError.style.display = 'block'
+		isValid = false
+	} else {
+		emailError.style.display = 'none'
+	}
+	isValid = passwordValidation(psw, pswError)
+	return isValid
+}
 
 document.addEventListener('DOMContentLoaded', () => {
 	email.addEventListener('input', validations.isRegister)
